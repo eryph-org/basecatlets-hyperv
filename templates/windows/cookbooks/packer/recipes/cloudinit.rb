402fbdcd76f37e59f1b3f9d6af57ed2462a811a2
Frank Wagner <frank2568@gmail.com>
2022-03-24 13:02:47 +0100

    use latest versions of cloudbase-init
    reason: stable was not released for a long time

--- templates/windows/cookbooks/packer/recipes/cloudinit.rb
+++ templates/windows/cookbooks/packer/recipes/cloudinit.rb
@@ -1,6 +1,6 @@
 
 windows_package 'cloudinit' do
-  source 'https://www.cloudbase.it/downloads/CloudbaseInitSetup_Stable_x64.msi'
+  source 'https://www.cloudbase.it/downloads/CloudbaseInitSetup_x64.msi'
 end
 
 cookbook_file 'C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf\cloudbase-init.conf' do
