de3e4ff6f682f56064c0f6bdce58ad99132f9a9f
Frank Wagner <frank2568@gmail.com>
2022-03-24 13:03:42 +0100

    added build all script

---
+++ build.ps1
@@ -0,0 +1,42 @@
+Push-Location $PSScriptRoot
+
+
+function Get-ObjectMember {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$True, ValueFromPipeline=$True)]
+        [PSCustomObject]$obj
+    )
+    $obj | Get-Member -MemberType NoteProperty | ForEach-Object {
+        $key = $_.Name
+        [PSCustomObject]@{Key = $key; Value = $obj."$key"}
+    }
+}
+
+$buildList = Get-Content -Raw -Path "build.json" | ConvertFrom-Json | Get-ObjectMember
+
+
+$buildList | ForEach-Object {
+    $osType = $_.Key
+    $variants = $_.Value
+        $variants | % {          
+            Join-Path ./templates $osType | Push-Location                   
+            try{            
+
+                $template = $_        
+                Write-Output "Build image $osType/$template"
+                ./build.ps1 -Template_name $template       
+                
+                
+            }
+            finally{
+                Pop-Location
+            }
+
+            .\tools\namingconvention.ps1 -BasePath .\builds -TemplateName $template
+        }
+
+
+   
+}
+
